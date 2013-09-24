1b4ada3034aabcfb086abfdf8d72e9ee772723bf
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-09-24 08:09:14 -0300

    Create setup file

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Flask-Quik
+--------------
+
+Flask quik integration.
+
+Links
+`````
+
+* `development version
+*   <http://github.com/avelino/flask-quik>`_
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-Quik',
+    version='0.1.1',
+    url='http://github.com/avelino/Flask-Quik',
+    license='MIT',
+    author='Thiago Avelino',
+    author_email='thiago@avelino.xxx',
+    description='Quik for Flask',
+    long_description=__doc__,
+    test_suite="nose.collector",
+    zip_safe=False,
+    platforms='any',
+    py_modules=['flask_quik'],
+    install_requires=['Flask'],
+    tests_require=['Flask-Testing', 'quik'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
